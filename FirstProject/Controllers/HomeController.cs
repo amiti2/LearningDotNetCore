bc2fa7fe31aaa42bd50454d9b0cf0be332934cbd
amiti2 <arcirodha1@gmail.com>
2022-01-21 18:25:23 +0530

    Razor engine basics

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning":"Good Evening";
+            return View("MyView2",viewModel);
+        }
+
+        public RedirectResult Index2()
+        {
+            return Redirect("http://google.com");
         }
     }
 }
