761838123763a0c9ceddcf3b13b8fe9417ab5c2d
amiti2 <arcirodha1@gmail.com>
2022-05-10 19:17:12 +0530

    updated git ignore

---
+++ Greeting App Solution/GreetingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿//welcome user to app
+Console.WriteLine("Welcome to Greeting Application");
+Console.WriteLine("This application was built by Amit");
+Console.WriteLine("-----------------------------------");
+Console.WriteLine();
+
+//ask for the first name
+Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+//greet user by their name
+Console.WriteLine("");
+Console.WriteLine("Hello "+ firstName);
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
