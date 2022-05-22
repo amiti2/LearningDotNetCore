b90472e6d1cc5df28c0008817a8c9adb5ee35a3e
amiti2 <arcirodha1@gmail.com>
2022-05-22 17:01:35 +0530

    init checkin

---
+++ Net5Assembly/CheckingNet5Assembly/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Serilog;
+namespace CheckingNet5Assembly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+     
+            Log.Information("Ah, there you are!");
+            Console.ReadLine();
+        }
+    }
+}
