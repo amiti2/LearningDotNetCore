f74a51097bd1a79398f6e56f06e7d5beb3c9cb0b
amiti2 <arcirodha1@gmail.com>
2022-05-24 13:57:37 +0530

    alt+0176 types degree symbol

--- VScodeFirstProject/Chapter01/Program.cs
+++ VScodeFirstProject/Chapter01/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Om Sai Ram!");
+System.Console.WriteLine("Temperature on {0:D} is {1}° C", DateTime.Now,23.4);
+//alt+0176  types degree symbol
---
+++ VScodeFirstProject/TestCommandHelp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestCommandHelp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int z;
+        }
+    }
+}
