7d10019700a8ab3a11824b97e351a492550872eb
amiti2 <arcirodha1@gmail.com>
2022-05-24 16:45:43 +0530

    DONE CHANGE

---
+++ VScodeFirstProject/Chapter02/reflectionexample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace reflectionexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Data.DataSet ds;
+            System.Net.Http.HttpClient client;   
+            System.Collections.Generic.List<Int16> J;   
+            Console.WriteLine("Hello World!");
+
+            foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                int methodCount = 0;
+                foreach(var t in a.DefinedTypes)
+                {
+                   methodCount += t.GetMethods().Count();
+                }
+
+                Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",arg0: a.DefinedTypes.Count(),arg1: methodCount,arg2: r.Name);
+            }
+        }
+    }
+}
