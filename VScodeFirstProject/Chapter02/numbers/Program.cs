f009b08c49cc5896bf7c352f2f23b0686a0d2ba1
amiti2 <arcirodha1@gmail.com>
2022-05-24 17:56:28 +0530

    adding binary with interger and using _

---
+++ VScodeFirstProject/Chapter02/numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint naturalNumber =23;
+            int number = -23;
+            int negtetest0 = -0;
+            uint testingNumber = 0;
+            int f= 4_444;
+
+            int binarynotation = 0b10_10;
+
+            int result = binarynotation+4;
+            Console.WriteLine(result);
+        }
+    }
+}
