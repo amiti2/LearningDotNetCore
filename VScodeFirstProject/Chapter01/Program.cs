e38afda5c167249574a33a1e13b717be26da51f0
amiti2 <arcirodha1@gmail.com>
2022-05-28 20:26:19 +0530

    1. learning sizeof operator 2. learing name of operator 3. checked unchecked 4. inside for loop maxvalue can be incremented with out throwing the    exception but anywhere else it will throw error

--- VScodeFirstProject/Chapter01/Program.cs
+++ VScodeFirstProject/Chapter01/Program.cs
@@ -1,4 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Om Sai Ram!");
-System.Console.WriteLine("Temperature on {0:D} is {1}° C", DateTime.Now,23.4);
+//Console.WriteLine("Om Sai Ram!");
+//System.Console.WriteLine("Temperature on {0:D} is {1}° C", DateTime.Now,23.4);
+//System.Console.WriteLine(int.MaxValue);
+//System.Console.WriteLine(int.MinValue);
+
+System.Console.WriteLine(byte.MaxValue + 2);
+System.Console.WriteLine(byte.MinValue);
+
+byte c =2;
+//try{
+
+unchecked{
+//c = Convert.ToByte(byte.MaxValue+c);
+}
+System.Console.WriteLine(c.GetType());
+//}
+// catch(Exception e)
+// {
+//     System.Console.WriteLine(c.GetType());
+// }
+
+System.Console.WriteLine($"value of c is {Convert.ToByte(c)}");
+       {
+        for(byte i=0;i<=byte.MaxValue; )
+            {
+               System.Console.WriteLine(i.GetType());
+                System.Console.WriteLine(i);   
+                i+= Convert.ToByte(2);            
+            }
+       }
+
 //alt+0176  types degree symbol
--- VScodeFirstProject/Chapter02/numbers/Program.cs
+++ VScodeFirstProject/Chapter02/numbers/Program.cs
@@ -4,6 +4,7 @@ namespace numbers
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             uint naturalNumber =23;
@@ -20,6 +21,29 @@ namespace numbers
             Console.WriteLine(result);
             System.Console.WriteLine(hexadecimalnotation ==binarynotation);
             System.Console.WriteLine(hexadecimalnotation ==10);
+            System.Console.WriteLine(nameof(hexadecimalnotation) +" is "+ hexadecimalnotation.GetType());
+            //System.Console.WriteLine(nameof(binarynotation) +"is"+ typeof(binarynotation));
+
+            double heightInMetres = 1.88;
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
+
+
+            System.Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in range {int.MaxValue:N0} and {int.MinValue:N0}");
+            System.Console.WriteLine($"Decimal uses {sizeof(decimal)} bytes and can store numbers in range {decimal.MaxValue:N0} and {decimal.MinValue:N0}");
+            System.Console.WriteLine($"Double uses {sizeof(double)} bytes and can store numbers in range {double.MaxValue:N0} and {double.MinValue:N0}");
+
+
+              
+        //     int total = 0;
+        //     checked
+        // {
+        //      for(byte i=0;i<byte.MaxValue; i= Convert.ToByte(2))
+        //     {
+        //         //total +=i;
+        //         System.Console.WriteLine(i + " " + total);
+        //     }
+        // }
+           
         }
     }
 }
