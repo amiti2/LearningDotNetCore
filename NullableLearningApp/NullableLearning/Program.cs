110ed08beddf573363f22e286b17357408ebb223
amiti2 <arcirodha1@gmail.com>
2022-06-12 19:45:59 +0530

    Binary Arithmatic operators

--- NullableLearningApp/NullableLearning/Program.cs
+++ NullableLearningApp/NullableLearning/Program.cs
@@ -4,6 +4,33 @@
     {
         static void Main(string[] args)
         {
+            // To run each example uncomment the subsequent statements
+
+            //PostAndPreIncrementExample();
+            //BinaryArithmaticOperatorsExample();
+
+        }
+
+        private static void BinaryArithmaticOperatorsExample()
+        {
+            int e = 11;
+            int f = 3;
+
+            Console.WriteLine($"e is {e} and f is {f}");
+            Console.WriteLine($"e + f = { e + f }");
+            Console.WriteLine($"e - f = { e - f }");
+            Console.WriteLine($"e * f = { e * f }");
+            Console.WriteLine($"e / f = { e / f }");
+            Console.WriteLine($"e % f = { e % f }" );
+
+            double g = 11.0;
+            Console.WriteLine($"g is {g:N1} and f is { f }");
+            Console.WriteLine($"g / f = { g / f }");
+
+        }
+
+        private static void PostAndPreIncrementExample()
+        {
             Console.WriteLine("Hello, World!");
 
             int a = 4;
@@ -22,8 +49,6 @@
             d = ++c; //d = 7 and c =7
 
             Console.WriteLine($"Value of c is {c} and d is {d}");
-
-
         }
     }
 }
