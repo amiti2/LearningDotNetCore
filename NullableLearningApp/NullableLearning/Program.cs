301c9d7382723b011deea6aa27d9c95d0631bd4b
amiti2 <arcirodha1@gmail.com>
2022-06-12 16:29:03 +0530

    1. Nullable practice 2. Pre increment and Post increment operator

---
+++ NullableLearningApp/NullableLearning/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace NullableLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int a = 4;
+            int b = a++;
+
+            Console.WriteLine($"Value of a is {a} and b is {b}.");
+            // b= 4 and a =5
+
+            int c = a;
+            int d = ++a;
+
+            //c = 5 and d = 6 and a is 6
+            Console.WriteLine($"Value of c is {c} and d is {d} and a is {a}.");
+
+            d = c++; //d = 5 and c =6
+            d = ++c; //d = 7 and c =7
+
+            Console.WriteLine($"Value of c is {c} and d is {d}");
+
+
+        }
+    }
+}
