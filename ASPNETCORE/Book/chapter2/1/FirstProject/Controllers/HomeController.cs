f031281b6d1afbb04ccce372e14a19aa902ff5ad
amiti2 <arcirodha1@gmail.com>
2022-07-04 15:52:55 +0530

    asp.net core practice

---
+++ ASPNETCORE/Book/chapter2/1/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstProject.Models;
+
+using System;
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good morning" : "Good Evening";
+            return View("MyView", viewModel);
+        }
+    }
+}
