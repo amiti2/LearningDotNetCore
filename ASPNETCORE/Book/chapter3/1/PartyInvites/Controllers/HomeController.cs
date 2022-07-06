866de286b16522de8ee0688989dc06c874255aab
amiti2 <arcirodha1@gmail.com>
2022-07-06 20:53:52 +0530

    Added chapter 3

---
+++ ASPNETCORE/Book/chapter3/1/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            //to do: store response from guest
+            return View();
+        }
+    }
+}
