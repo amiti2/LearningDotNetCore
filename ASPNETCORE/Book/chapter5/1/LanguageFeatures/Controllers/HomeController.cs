0792660305c2c5be2b94b0c62d397534cd83ee22
amiti2 <arcirodha1@gmail.com>
2022-07-09 03:26:05 +0530

    Dictionary related changed

--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
@@ -6,24 +6,17 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-          
-            List<string> results = new List<string>();
-            foreach(var product in Product.GetProducts())
-            {
-                string name = product?.Name ?? "No Name";
-                decimal price = product?.Price ?? 0.25M;
-                string relatedName = product?.Related?.Name ?? "No Related Product Name";
-                string category = product?.Category;
-                bool inStock = product?.Instock ?? false;
-               
-                results.Add(string.Format("Name: {0, 80}, Price:{1 , 5  }, Related:{2}, Category : {3}, In stock = {4}", name, price, relatedName, category, inStock));
-            }
+            Dictionary<string, Product> products = new Dictionary<string, Product> {
+                ["kayak"] = new Product { Name = "Kayak" , Price = 275M },
+                ["Life Jacket"] = new Product {  Name="Life Jacket", Price = 49.75M}
+            };
 
-            return results[0];
-            //return View(results);
-            //return View(new string[] { "C#", "Language", "Features" });
+
+           
+
+            return View(products.Keys);
         }
     }
 }
