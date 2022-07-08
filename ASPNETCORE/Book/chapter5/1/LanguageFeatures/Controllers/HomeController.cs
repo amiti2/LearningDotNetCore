3a4a3911551c8f998f2b78867c7c27a05574f490
amiti2 <arcirodha1@gmail.com>
2022-07-08 23:37:39 +0530

    null coalesing and null conditional operator usage

--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
@@ -11,9 +11,10 @@ namespace LanguageFeatures.Controllers
             List<string> results = new List<string>();
             foreach(var product in Product.GetProducts())
             {
-                string name = product?.Name;
-                decimal? price = product?.Price;
-                results.Add(string.Format("Name: {0}, Price:{1}", name, price));
+                string name = product?.Name ?? "No Name";
+                decimal price = product?.Price ?? 0.25M;
+                string relatedName = product?.Related?.Name ?? "No Related Product Name";
+                results.Add(string.Format("Name: {0}, Price:{1}, Related:{2}", name, price, relatedName));
             }
             return View(results);
             //return View(new string[] { "C#", "Language", "Features" });
--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Models/Product.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Models/Product.cs
@@ -5,10 +5,14 @@
         public string Name { get; set; }
         public decimal? Price { get; set; }
 
+        public Product Related { get; set; }
+
         public static Product[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Price = 275M };
             Product LifeJacket = new Product { Name="Life Jacket", Price = 48.95M};
+            kayak.Related = LifeJacket;
+
             return new Product[] { kayak, LifeJacket, null };
 
         }
