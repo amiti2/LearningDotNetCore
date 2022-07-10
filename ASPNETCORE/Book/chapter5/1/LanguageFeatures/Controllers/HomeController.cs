31d960fb20f24406e6444a4ff5d3bd3fbaec0b04
amiti2 <arcirodha1@gmail.com>
2022-07-10 22:40:48 +0530

    learning extension methods
    extension methods temporary changes

--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
@@ -8,15 +8,26 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            Dictionary<string, Product> products = new Dictionary<string, Product> {
-                ["kayak"] = new Product { Name = "Kayak" , Price = 275M },
-                ["Life Jacket"] = new Product {  Name="Life Jacket", Price = 49.75M}
+            Dictionary<string, Product> products = new Dictionary<string, Product>
+            {
+                ["kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Life Jacket"] = new Product { Name = "Life Jacket", Price = 49.75M }
             };
 
 
-           
+            object[] data = new object[6] { 22M, 29.5M, "Adidad", "Nike", 3 ,5 };
 
-            return View(products.Keys);
+            decimal total = default;
+
+            for(int i=0;i <data.Length; i++)
+            {
+                if (data[i] is decimal d)
+                {
+                    total += d;
+                }
+            }
+
+            return View(new string[] { $"Total :  {total:c2} " });
         }
     }
 }
---
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart p)
+        {
+            decimal total = 0;
+            foreach (Product product in p.Products)
+            {
+
+            }
+
+        }
+
+    }
+}
