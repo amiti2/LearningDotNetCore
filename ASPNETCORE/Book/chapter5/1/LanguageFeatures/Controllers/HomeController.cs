15634d696252d57748b53ddb5b153f3640036d79
amiti2 <arcirodha1@gmail.com>
2022-07-12 18:19:55 +0530

    using delegate

--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Controllers/HomeController.cs
@@ -1,33 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using LanguageFeatures.Models;
+using System;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+
+        static bool FilterByPrice(Product p)
+        {
+            return p.Price > 20;
+        }
+
+        static bool FilterByName(Product p)
+        {
+            return p.Name.StartsWith("S");
+        }
+
+        
         public ViewResult Index()
         {
-            Dictionary<string, Product> products = new Dictionary<string, Product>
+            //Dictionary<string, Product> products = new Dictionary<string, Product>
+            //{
+            //    ["kayak"] = new Product { Name = "Kayak", Price = 275M },
+            //    ["Life Jacket"] = new Product { Name = "Life Jacket", Price = 49.75M }
+            //};
+
+
+            //object[] data = new object[6] { 22M, 29.5M, "Adidad", "Nike", 3 ,5 };
+
+            //decimal total = default;
+
+            //for(int i=0;i <data.Length; i++)
+            //{
+            //    if (data[i] is decimal d)
+            //    {
+            //        total += d;
+            //    }
+            //}
+
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            //decimal cartTotal = cart.TotalPrices();
+            //return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+
+            //ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+
+            //Product[] productArray = {  
+            //        new Product {Name = "Kayak", Price = 275M},
+            //        new Product {Name = "Lifejacket", Price = 48.95M}
+            //        };
+
+            //decimal cartTotal = cart.TotalPrices();
+            //decimal arrayTotal = productArray.TotalPrices();
+
+            //return View("Index", new string[] {
+            //                        $"Cart Total: {cartTotal:C2}",
+            //                        $"Array Total: {arrayTotal:C2}" });
+
+            Func<Product, bool> NameFilter = delegate (Product p)
             {
-                ["kayak"] = new Product { Name = "Kayak", Price = 275M },
-                ["Life Jacket"] = new Product { Name = "Life Jacket", Price = 49.75M }
+                return p.Name.StartsWith("S");
             };
 
+            Product[] productArray = {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+};
+            //decimal arrayTotal = productArray.FilterPrice(20).TotalPrices();
 
-            object[] data = new object[6] { 22M, 29.5M, "Adidad", "Nike", 3 ,5 };
+            //decimal nameFilterTotal = productArray.FilterByName("C").TotalPrices();
 
-            decimal total = default;
+            decimal arrayTotal = productArray.Filter(FilterByPrice).TotalPrices();
+            //decimal nameFilterTotal = productArray.Filter(FilterByName).TotalPrices();
 
-            for(int i=0;i <data.Length; i++)
-            {
-                if (data[i] is decimal d)
-                {
-                    total += d;
-                }
-            }
+            decimal nameFilterTotal = productArray.Filter(NameFilter).TotalPrices();
 
-            return View(new string[] { $"Total :  {total:c2} " });
+            return View("Index", new string[] { $"Array Total: {arrayTotal:C2}" , $"Array Total: { nameFilterTotal:C2}" });
         }
     }
 }
--- ASPNETCORE/Book/chapter5/1/LanguageFeatures/Models/MyExtensionMethods.cs
+++ ASPNETCORE/Book/chapter5/1/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿namespace LanguageFeatures.Models
+﻿using System;
+using System.Collections.Generic;
+namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods
     {
@@ -7,9 +9,62 @@
             decimal total = 0;
             foreach (Product product in p.Products)
             {
-
+                total += product?.Price ?? 0;
             }
 
+            return total;
+        }
+
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+
+            decimal total = 0;
+           foreach (Product product in products)
+            {
+                total += product?.Price ?? 0;
+            }
+
+            return total;
+        }
+
+        public static IEnumerable<Product> Filter ( this IEnumerable<Product> products, Func<Product, bool> selector )
+        {
+            foreach (Product product in products)
+            {
+                if (selector(product))
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        
+
+        public static IEnumerable<Product> FilterPrice(this IEnumerable<Product> p, decimal minimum )
+        {
+            foreach(Product product in p)
+            {
+                if(product.Price > minimum)
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> FilterByName(this IEnumerable<Product> products, string name)
+        {
+            foreach(Product product in products)
+            {
+                if(product.Name.StartsWith(name))
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        public static string ToString1(this Product p)
+        {
+            return ($"{p.Name}- { p.Price}");
         }
 
     }
