d7fb895b565c454bb978108b9ccb1dcd75d8e7d0
amiti2 <arcirodha1@gmail.com>
2022-07-23 19:52:33 +0530

    1. If you have only httpget httppost over the method then end point will be http://localhost//{nameofcontroller}
    2. You can not have public method in controller without [http..] attirbute. It will compile but swagger will be confused so it will not render properly
    
    3. to make it work you need to mark method as private.

--- Learning_web_Api/ApiSecurityApp/ApiSecurity/Controllers/AuthenticationController.cs
+++ Learning_web_Api/ApiSecurityApp/ApiSecurity/Controllers/AuthenticationController.cs
@@ -7,5 +7,39 @@ namespace ApiSecurity.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        [HttpPost]
+        public ActionResult<string> Authenticate([FromBody] string password)
+        {
+            return password;
+        }
+
+        #region "Test APIs"
+        //[HttpGet]
+        //public ActionResult<List<string>> Get()
+        //{
+        //    List<string> users = new();
+        //    users.Add("amit");
+        //    users.Add("rajan");
+        //    return users;
+        //}
+
+        //this will break swagger. Move it to private
+        //public ActionResult<List<string>> Put()
+        //{
+        //    List<string> users = new();
+        //    users.Add("amit");
+        //    users.Add("rajan");
+        //    return users;
+        //}
+
+        //[HttpGet(template:"aa")]
+        //public ActionResult<List<string>> Get2()
+        //{
+        //    List<string> users = new();
+        //    users.Add("amit");
+        //    users.Add("rajan");
+        //    return users;
+        //}
+        #endregion
     }
 }
